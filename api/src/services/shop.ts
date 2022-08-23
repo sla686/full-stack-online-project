b9645eeb75ec51bbdfd1e67db7f66f3e4b8e985c
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-08-23 11:14:37 +0300

    added missed await keyword

--- api/src/services/shop.ts
+++ api/src/services/shop.ts
@@ -4,7 +4,7 @@ import Shop, { ShopDocument } from '../models/Shop'
 import { NotFoundError, BadRequestError } from '../helpers/apiError'
 
 const create = async (shop: ShopDocument): Promise<ShopDocument> => {
-  return shop.save()
+  return await shop.save()
 }
 
 export default { create }
