e1927b7a99cc533c7b71f7b729258fcd0e193d40
Viacheslav Semushin <sla686@gmail.com>
2022-08-25 16:36:41 +0300

    playing with theme palette colors

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,17 +1,32 @@
 import { createTheme } from '@mui/material/styles'
 
 const theme = createTheme({
+  // palette: {
+  //   primary: {
+  //     light: '#8eacbb',
+  //     main: '#607d8b',
+  //     dark: '#34515e',
+  //     contrastText: '#fff',
+  //   },
+  //   secondary: {
+  //     light: '#e7ff8c',
+  //     main: '#b2ff59',
+  //     dark: '#7ecb20',
+  //     contrastText: '#000',
+  //   },
+  //   mode: 'light',
+  // },
   palette: {
     primary: {
-      light: '#8eacbb',
-      main: '#607d8b',
-      dark: '#34515e',
+      light: '#5c67a3',
+      main: '#3f4771',
+      dark: '#2e355b',
       contrastText: '#fff',
     },
     secondary: {
-      light: '#e7ff8c',
-      main: '#b2ff59',
-      dark: '#7ecb20',
+      light: '#ff79b0',
+      main: '#ff4081',
+      dark: '#c60055',
       contrastText: '#000',
     },
     mode: 'light',
