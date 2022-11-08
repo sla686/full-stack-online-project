c679436ab172509226b3efe9529b77db3359a094
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-11-08 16:24:06 +0300

    testing

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -4,6 +4,6 @@ import App from '../app/App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/marketplace & shops/i)
+  const linkElement = screen.getByText(/marketplace/i)
   expect(linkElement).toBeInTheDocument()
 })
