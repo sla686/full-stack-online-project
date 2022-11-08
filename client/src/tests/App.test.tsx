172a95ed792586629c9bcdbb713967e22c15f920
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-11-08 16:36:38 +0300

    small fixes

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import App from '../app/App'
 
 test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/marketplace/i)
+  console.log(linkElement)
   expect(linkElement).toBeInTheDocument()
 })
