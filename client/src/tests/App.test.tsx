392fc6a646d62fa6dad528ccd12213a08bfdaca3
Viacheslav Semushin <bvise001@edu.xamk.fi>
2022-11-08 16:53:17 +0300

    testing commit

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -4,9 +4,9 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import App from '../app/App'
 
-test('renders learn react link', () => {
+test('renders marketplace & shops title', () => {
   render(<App />)
-  const linkElement = screen.getByText(/marketplace/i)
+  const linkElement = screen.getByText(/marketplace & shops/i)
   console.log(linkElement)
   expect(linkElement).toBeInTheDocument()
 })
