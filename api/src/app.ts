8412b7d0fe0351c1b37ebd1e4d663e73c21c5075
Viacheslav Semushin <sla686@gmail.com>
2023-05-07 19:12:32 +0300

    Allowing all origins

--- api/src/app.ts
+++ api/src/app.ts
@@ -28,22 +28,23 @@ const allowedOrigins = [
   'http://localhost:4000',
   'https://fullstack-online-shop.netlify.app',
 ]
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true)
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          'The CORS policy for this site does not allow access from the specified Origin.'
-        return callback(new Error(msg), false)
-      }
-      return callback(null, true)
-    },
-    credentials: true,
-  })
-)
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true)
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg =
+//           'The CORS policy for this site does not allow access from the specified Origin.'
+//         return callback(new Error(msg), false)
+//       }
+//       return callback(null, true)
+//     },
+//     credentials: true,
+//   })
+// )
+app.use(cors())
 
 // Set up routers
 app.use('/api/v1/users', userRouter)
